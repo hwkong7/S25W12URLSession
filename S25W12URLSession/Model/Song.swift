b653107289221ca31215e57247424fb15ca58fca
신혜원 <hwkong7_@naver.com>
2025-11-24 16:19:25 +0900

    🔐API Key

---
+++ S25W12URLSession/Model/Song.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Song: Identifiable, Decodable {
+    let id: UUID
+    let title: String
+    let singer: String
+    let rating: Int
+    let lyrics: String?
+    
+}
+
+
