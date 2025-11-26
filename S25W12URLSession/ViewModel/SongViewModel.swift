395230ebf844051ad99f38e36d9079f0f69247c3
신혜원 <hwkong7_@naver.com>
2025-11-26 14:33:03 +0900

    🗃️addSong

--- S25W12URLSession/ViewModel/SongViewModel.swift
+++ S25W12URLSession/ViewModel/SongViewModel.swift
@@ -17,4 +17,20 @@ final class SongViewModel {
     func loadSongs() async {
         _songs = try! await repository.fetchSongs()
     }
+    
+    // func addSong(newSong song: Song)
+    // newSong은 외부 매개변수 이름, song은 함수 내부에서 사용하는 매개변수 이름
+    // viewModel.addSong(newSong: song)
+    //
+    // _는 외부 매개변수 이름을 사용하지 않겠다는 표현
+    // viewModel.addSong(song)
+    func addSong(_ song: Song) async {
+        do {
+            try await repository.saveSong(song)
+            _songs.append(song)
+        }
+        catch {
+            debugPrint("에러 발생: \(error)")
+        }
+    }
 }
