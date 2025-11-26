319e1cfe2c0a41bab55301419e97e96bf0ca7275
신혜원 <hwkong7_@naver.com>
2025-11-26 14:47:06 +0900

    ➕노래 추가

--- S25W12URLSession/ContentView.swift
+++ S25W12URLSession/ContentView.swift
@@ -1,3 +1,5 @@
+@Environment(\.dismiss) var dismiss
+
 import SwiftUI
 
 struct ContentView: View {
@@ -19,6 +21,7 @@ struct ContentView: View {
 
 struct SongView: View {
     @State private var viewModel = SongViewModel()
+    @State private var showingAddSheet = false
     
     var body: some View {
         NavigationStack(path: $viewModel.path) {
@@ -32,6 +35,16 @@ struct SongView: View {
             }
             .refreshable {
                 await viewModel.loadSongs()
+            }
+            .toolbar {
+                Button {
+                    showingAddSheet.toggle()
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                }
+            }
+            .sheet(isPresented: $showingAddSheet) {
+                SongAddView(viewModel: viewModel)
             }
         }
     }
@@ -54,11 +67,11 @@ struct SongListView: View {
                 }
             }
         }
-        .task {
-            await viewModel.addSong(
-                Song(id: UUID(), title: "test", singer: "singer", rating: 1, lyrics: "lyrics")
-            )
-        }
+//        .task {
+//            await viewModel.addSong(
+//                Song(id: UUID(), title: "test", singer: "singer", rating: 1, lyrics: "lyrics")
+//            )
+//        }
     }
 }
 
@@ -93,6 +106,62 @@ struct SongDetailView: View {
 struct SingerView: View {
     var body: some View {
         Text("Singer View")
+    }
+}
+
+struct SongAddView: View {
+    let viewModel: SongViewModel
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State var title = ""
+    @State var singer = ""
+    @State var rating = 3
+    @State var lyrics = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("노래 정보 *")) {
+                    TextField("제목", text: $title)
+                    TextField("가수", text: $singer)
+                }
+                
+                Section(header: Text("선호도 *")) {
+                    Picker("별점", selection: $rating) {
+                        ForEach(1...5, id: \.self) { score in
+                            Text("\(score)점")
+                                .tag(score)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section(header: Text("가사")) {
+                    TextEditor(text: $lyrics)
+                        .frame(height: 150)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("추가") {
+                        Task {
+                            await viewModel.addSong(
+                                Song(id: UUID(), title: title, singer: singer, rating: rating, lyrics: lyrics)
+                            )
+                            dismiss()
+                        }
+                    }
+                    .disabled(title.isEmpty || singer.isEmpty)
+                }
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("취소") {
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 
